835c89c165808758cb2f6e318b32a393e6e3c1da
Dennis Schulmeister-Zimolong <dennis@patk.org>
2019-09-30 07:33:34 +0200

    Neue Aufgabe nochmal angepasst

--- 3-javascript/Grundstruktur einer Single Page App/Aufgabe/page-overview/page-overview.js
+++ 3-javascript/Grundstruktur einer Single Page App/Aufgabe/page-overview/page-overview.js
@@ -16,27 +16,6 @@ class PageOverview {
      * Seite anzeigen. Wird von der App-Klasse aufgerufen.
      */
     async show() {
-        // Anzuzeigenden Seiteninhalt nachladen
-        let html = await fetch("page-overview/page-overview.html");
-        let css = await fetch("page-overview/page-overview.css");
-
-        if (html.ok && css.ok) {
-            html = await html.text();
-            css = await css.text();
-        } else {
-            console.error("Fehler beim Laden des HTML/CSS-Inhalts")
-            return;
-        }
-
-        // Seite zur Anzeige bringen
-        let pageDom = document.createElement("div");
-        pageDom.innerHTML = html;
-
-        // TODO: Kacheln zum Aufruf der Detailseiten rendern
-
-        this._app.setPageTitle("Startseite");
-        this._app.setPageCss(css);
-        this._app.setPageHeader(pageDom.querySelector("header"));
-        this._app.setPageContent(pageDom.querySelector("main"));
+        // TODO: Seite anzeigen
     }
 }
