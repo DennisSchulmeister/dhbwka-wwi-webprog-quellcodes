fde21ee51d543cb161d8b08256ab35414ee7ed3f
Dennis Schulmeister-Zimolong <dennis@patk.org>
2021-09-20 06:46:19 +0200

    Neue Fallbeispiele für Responsive Webdesign

---
+++ 2 Responsive Webdesign/Beispiele/Unterschiedliche Anordnung/example-data.js
@@ -0,0 +1,27 @@
+examples = [
+    {
+        "img":  "img/dog1.jpg",
+        "name": "Ernesto",
+        "text": "Ernesto ist wirklich ein liebenswertes Kerlchen. Dieser kleine Welpe verträgt sich prima mit Kindern und anderen Hunden."
+    }, {
+        "img":  "img/dog2.jpg",
+        "name": "Friedhelm",
+        "text": "Unser jüngster Neuzugang ist verspielt immer gut gelaunt. Am liebsten tobt er im Garten mit den anderen Hunden. Friedhelm muss man einfach lieb haben."
+    }, {
+        "img":  "img/dog3.jpg",
+        "name": "Jacko",
+        "text": "Schwarzes Fell und seriöser Blick. Das ist Jacko. In Wirklichkeit ist er aber ein ganz lieber Familienhund, der alles für seine Familie geben würde."
+    }, {
+        "img":  "img/dog4.jpg",
+        "name": "Danni",
+        "text": "Danni ist eine ganz besondere kleine Hundedame. Sie weiß, was sie will und gibt nicht nach, bis sie es hat. Aber wer könnte diesem Blick schon wiederstehen?"
+    }, {
+        "img":  "img/dog5.jpg",
+        "name": "Rudi",
+        "text": "Unser Rudi ist immer auch der Hut. Nicht das kleinste Geräusch entgeht ihm und schon steht er da. Bei ihm kommt niemand unbemerkt ins Haus."
+    }, {
+        "img":  "img/dog6.jpg",
+        "name": "Tina",
+        "text": "Sehr verspielt und immer für einen Spaß zu haben. Unsere Tina bringt Freude und Leben auch an grauen Tagen. Wie könnte man sie nicht sofort ins Herz schließen?"
+    },
+];
