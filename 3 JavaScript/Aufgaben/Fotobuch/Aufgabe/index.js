a5774f5dc41f683b9ea9bf85e5ce8fd094f531de
Dennis Schulmeister-Zimolong <dennis@patk.org>
2021-10-18 05:44:13 +0200

    Neue Aufgabe: Fotobuch

---
+++ 3 JavaScript/Aufgaben/Fotobuch/Aufgabe/index.js
@@ -0,0 +1,104 @@
+window.addEventListener("load", async () => {
+    let images = document.querySelectorAll("link[rel=preload][as=image]");
+    let backgroundContainer = document.querySelector("#background");
+    let previewContainer = document.querySelector("#preview");
+
+    /**
+     * Hilfsfunktion, zur einfacheren Nutzung von window.setTimeout().
+     * Hierdurch kein eine Callback-Funktion eingespart werden, indem statt
+     *
+     *   window.setTimeout(() => { ... }, zeit);
+     *
+     * Folgender Quellcode verwendet wird:
+     *
+     *   await sleep(zeit);
+     *   ...
+     *
+     * Die rufende Funktion muss hierfür mit async gekennzeichnet werden.
+     *
+     * @param {Number} ms Abzuwartende Zeit in Milisekunden
+     */
+    async function sleep(ms) {
+        return new Promise(resolve => {
+            window.setTimeout(() => resolve(), ms);
+        });
+    }
+
+    /**
+     * Wechsel des gerade sichtbaren Hintergrundbilds. Der Übgergang wird
+     * mit Hilfe von CSS animiert.
+     *
+     * @param {Number} index Index im images-Array
+     */
+    async function showImage(index) {
+        // TODO 4: Zunächst sollen alle Kinder des backgroundContainer gesucht
+        // in in einer lokalen Variable zwischengespeichert werden. Sie werden
+        // später noch gebraucht. In einer zusätzliche Boolean-Variable soll
+        // gespeichert werden, ob etwas gefunden wurde und somit später eine
+        // Animation zur Überblendung des Bildes angezeigt werden soll.
+        //
+        // Das neue Hintergrundbild soll als <div>-Element dem backgroundContainer
+        // hinzugefügt werden. Damit das Bild immer korrekt skaliert und zentriert
+        // wird, muss es als Hintergrundbild des <div>-Elements gesetzt werden.
+        // Das heißt, das Attribut style.backgroundImage zum Manipulieren der
+        // CSS-Anweisung background-image muss mit dem richtigen Wert versorgt
+        // werden.
+        //
+        // Die URL des sichtbaren Bilds kann anhand der <link>-Elemente aus dem
+        // images-Array ermittelt werden. Dabei muss nur beachtet werden, dass
+        // die URL im href-Attribut eines <link>-Elements steht.
+        //
+        // Im previewContainer soll das ausgewählte Bild mit der Klasse "current"
+        // zugewiesen bekommen, damit es farblich hervorgehoben wird. Den anderen
+        // Vorschaubildern muss die Klasse natürlich entfernt werden. Ebenso soll
+        // den anderen Vorschaubildern die im Folgenden beschriebene Klasse "visible"
+        // entfernt werden.
+        //
+        // Damit in der mobilen Ansicht immer genau drei Vorschaubilder sichtbar
+        // sind, soll dem vorherigem und das nachfolgendem Bild die Klasse
+        // "visible" zugewiesen werden. Jedoch wird eine Sonderbehandlung für
+        // den Fall index == 0 und index == images.length - 1 benötigt, da das
+        // erste und letzte Bild ja keinen Vorgänger bzw. Nachfolger besitzen,
+        // aber dennoch drei Bilder angezeigt werden sollen.
+        //
+        // Schlussendlich soll der Wechsel des Hintergrundbilds animiert werden,
+        // falls ganz am Anfang der Methode ein schon angezeigtes Bild gefunden
+        // wurde. Hierbei muss wie folgt vorgegangen werden:
+        //
+        //   1. Dem <div> mit dem neuen Bild die Klasse "fadeout" geben.
+        //
+        //   2. 50 Milisekunden warten, damit die CSS-Regeln wirklich greifen.
+        //
+        //   3. Dem <div> die Klasse "fadeout" wieder wegnehmen.
+        //
+        //   4. Über alle am Anfang der Methode gefundenen, alten Inhalte
+        //      eine Schleife bilden und diesen ebenfalls die Klasse "fadeout"
+        //      zuweisen. Zusätzlich auf das Event "transitionend" der Elemente
+        //      reagieren, um die Elemente nach Ablauf der Animation aus dem
+        //      DOM zu entfernen.
+    }
+
+    // Auswahlelemente zum Umschalten des Bilds generieren:
+    for (let i = 0; i < images.length; i++) {
+        // TODO 2: Für jedes Bild soll ein <img>-Element erzeugt und dem previewContainer
+        // hinzugefügt werden.
+        //
+        // Das Attribut dataset.index des <img>-Elements soll mit der Nummer des Bildes
+        // aus der Variable i versorgt werden, damit bei einem Klick auf das Vorschaubild
+        // diese an die Methode showImage() übergeben werden kann. Hierbei muss allerdings
+        // beachtet werden, dass der im dataset.index-Attribut gespeicherte Wert immer in
+        // einen String umgewandelt wird und daher vor dem Aufruf von showImage() mit
+        // parseInt() erst wieder in einen Integer umgewandelt werden muss.
+        //
+        // Das Array images enthält die <link>-Elemente aus dem Header, mit denen die
+        // anzuzeigenden Bilder definiert und vorgeladen werden. Die URL ist daher über
+        // das href-Attribut zu ermitteln, muss aber dem src-Attribut des <img>-Elements
+        // übergeben werden.
+        //
+        // Wenn das angeklickte Bild die Klasse "current" besitzt, soll nichts passieren.
+        // Andernfalls soll das angeklicke Bild mit showImage() wie oben beschrieben
+        // angezeigt werden.
+    };
+
+    // TODO 3: Erstes Bild anzeigen
+});
---
+++ 3 JavaScript/Aufgaben/Fotobuch/Loesung/index.js
@@ -0,0 +1,92 @@
+window.addEventListener("load", async () => {
+    let images = document.querySelectorAll("link[rel=preload][as=image]");
+    let backgroundContainer = document.querySelector("#background");
+    let previewContainer = document.querySelector("#preview");
+
+    /**
+     * Hilfsfunktion, zur einfacheren Nutzung von window.setTimeout().
+     * Hierdurch kein eine Callback-Funktion eingespart werden, indem statt
+     *
+     *   window.setTimeout(() => { ... }, zeit);
+     *
+     * Folgender Quellcode verwendet wird:
+     *
+     *   await sleep(zeit);
+     *   ...
+     *
+     * Die rufende Funktion muss hierfür mit async gekennzeichnet werden.
+     *
+     * @param {Number} ms Abzuwartende Zeit in Milisekunden
+     */
+    async function sleep(ms) {
+        return new Promise(resolve => {
+            window.setTimeout(() => resolve(), ms);
+        });
+    }
+
+    /**
+     * Wechsel des gerade sichtbaren Hintergrundbilds. Der Übergang wird
+     * mit Hilfe von CSS animiert.
+     *
+     * @param {Number} index Index im images-Array
+     */
+    async function showImage(index) {
+        // Vorhandenes Hintergrundbild ermitteln
+        let existingElements = backgroundContainer.querySelectorAll("*");
+        let animateTransition = existingElements.length > 0;
+
+        // Neues Hintergrundbild einfügen
+        let divElement = document.createElement("div");
+        divElement.style.backgroundImage = `url(${images[index].href})`;
+        backgroundContainer.appendChild(divElement);
+
+        // Dazugehöriges Vorschaubild hervorheben und sicherstellen, dass in
+        // der mobilen Ansicht auch das Bild davor und danach ausgewählt werden
+        // kann. In der Mobilansicht werden hierfür immer exkt drei Vorschau-
+        // Bilder angezeigt.
+        for (let previewImage of previewContainer.querySelectorAll("img")) {
+            previewImage.classList.remove("visible");
+            previewImage.classList.remove("current");
+        }
+
+        previewContainer.querySelector(`[data-index='${index - 1}']`)?.classList.add("visible");
+        previewContainer.querySelector(`[data-index='${index + 0}']`)?.classList.add("current");
+        previewContainer.querySelector(`[data-index='${index + 1}']`)?.classList.add("visible");
+
+        if (index == 0) {
+            previewContainer.querySelector(`[data-index='${index + 2}']`)?.classList.add("visible");
+        } else if (index == images.length - 1) {
+            previewContainer.querySelector(`[data-index='${index - 2}']`)?.classList.add("visible");
+        }
+
+        // Keine Überblendung beim ersten Aufruf, da in diesem Fall
+        // noch kein altes Hintergrundbild sichtbar ist und ein Einblenden
+        // beim Laden der Seite doof aussieht.
+        if (!animateTransition) return;
+
+        divElement.classList.add("fadeout");
+        await sleep(50);
+        divElement.classList.remove("fadeout");
+
+        for (let existingElement of existingElements) {
+            existingElement.addEventListener("transitionend", event => event.target.remove());
+            existingElement.classList.add("fadeout");
+        }
+    }
+
+    // Auswahlelemente zum Umschalten des Bilds generieren
+    for (let i = 0; i < images.length; i++) {
+        let imgElement = document.createElement("img");
+        imgElement.src = images[i].href;
+        imgElement.dataset.index = i;
+        previewContainer.appendChild(imgElement);
+
+        imgElement.addEventListener("click", () => {
+            if (imgElement.classList.contains("current")) return;
+            showImage(parseInt(imgElement.dataset.index));
+        });
+    };
+
+    // Erstes Bild anzeigen
+    showImage(0);
+});
